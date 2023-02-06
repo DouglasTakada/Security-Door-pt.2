3ba02e5dbc0d61db6e62053e5c7f224625c5bc98
DouglasTakada <douglas.takada@gmail.com>
2023-02-06 00:41:33 -0800

    Compiles&executes but keeps thinking im Arnold :(

--- Face Recognition/face-train.py
+++ Face Recognition/face-train.py
@@ -1,13 +1,18 @@
 import cv2
 import os
+import pickle
 from PIL import Image
 import numpy as np
 
+
 #gets path of this file
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR,"Douglas")
+image_dir = os.path.join(BASE_DIR,"images")
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+#if above function says cv2 dont have .face function you gotta do:
+#pip install opencv-contrib-python
 
 #training labels
 current_id = 0
@@ -21,7 +26,7 @@ for root, dirs, files in os.walk(image_dir):
             path = os.path.join(root,file)
             label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
             #Grabs and prints path to all photos it found in the directory Douglas
-            print(path)
+            #print(path)
 
             if not label in label_ids:
                 label_ids[label] = current_id
@@ -34,12 +39,23 @@ for root, dirs, files in os.walk(image_dir):
             pil_image = Image.open(path).convert("L") # converts image into gray scale
             #Convert image into number array using numpy. It is taking every pixle value and converting
             #it to numbers in the array
-            image_array = np.array(pil_image,"uint8")
+            size = (550,500)
+            final_image = pil_image.resize(size,Image.ANTIALIAS)
+            image_array = np.array(final_image,"uint8")
             print(image_array)
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5,minNeighbors =5)
 
             for(x,y,w,h) in faces:
-                foi = image_array[y:y+h,x:x+w]
+                roi = image_array[y:y+h,x:x+w]
                 x_train.append(roi)
+                y_labels.append(id_)
             
 
+#save labels so we can use inside other py script
+#going to use pickle for this
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids,f)
+
+#now train item itself makin facial recognizer
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainner.yml")
--- Face Recognition/face.py
+++ Face Recognition/face.py
@@ -1,7 +1,17 @@
 import numpy as np
 import cv2
+import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {}
+with open("labels.pickle", 'rb') as f:
+    Original_labels = pickle.load(f)
+    labels = {v:k for k,v in Original_labels.items()}#invserting the pair so it is proper format
 
 cap = cv2.VideoCapture(0)
 
@@ -10,16 +20,26 @@ while True:
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
     for(x,y,w,h) in faces:
-        print(x,y,w,h)
+        #print(x,y,w,h)
         #reigon of interest = roi
         #[y:y+h,x:x+w] find the box of the face and the imwrite writes the image in the png
         roi_gray = gray[y:y+h,x:x+w]
         roi_color = frame[y:y+h,x:x+w]
 
         #How to recognize? deep learned model predict keras, tensorflow, pytorch ,scikit, learn
+        id_, conf = recognizer.predict(roi_gray)
 
-        img_item = "my-image.png"
-        cv2.imwrite(img_item, roi_gray)
+        img_item = "1.png"
+        cv2.imwrite(img_item, roi_color)
+        if conf >=45 and conf <=100:
+            print(conf)
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255,255,255)#white
+            stroke =2
+            cv2.putText(frame,name, (x,y),font, 1, color,stroke, cv2.LINE_AA)
 
         #creating rectangle for around face lol
         color = (255,0,0 )#Blue Green Red not RGB
@@ -27,6 +47,14 @@ while True:
         end_cord_x = x + w
         end_cord_y = y + h
         cv2.rectangle(frame,(x,y),(end_cord_x, end_cord_y), color, stroke)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+        
+        # mouth = eye_cascade.detectMultiScale(roi_gray)
+        # for (mx,my,mw,mh) in mouth:
+        #     cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,0,255),2)
 
 
     cv2.imshow('frame',frame)
